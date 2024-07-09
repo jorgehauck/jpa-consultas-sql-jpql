6be98a926ba5dbfcea699a736afb625d82657411
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2024-07-08 21:07:27 -0300

    BCrypt password

--- spring-boot-oauth2-jwt-demo/password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ spring-boot-oauth2-jwt-demo/password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$q1.dCNx7DUwsu5PqAWxsDeBJPGX8OheEs8xmOg9T2Vu22TxKgW/p.');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$q1.dCNx7DUwsu5PqAWxsDeBJPGX8OheEs8xmOg9T2Vu22TxKgW/p.');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
